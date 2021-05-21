5c7f39abbc0d487aa027dad863c919c2b7426c2c
Nathan Tai <nathantai@outlook.com>
2021-05-21 14:22:39 +0800

    add hooks/useScreen.js

---
+++ src/hooks/useScreen.js
@@ -0,0 +1,18 @@
+import { ref, onMounted,nextTick } from 'vue'
+
+export function useScreen(id) {
+    const width = ref(0)
+    const height = ref(0)
+
+    onMounted(() => {
+        const dom = document.getElementById(id)
+        width.value = dom.clientWidth
+        height.value = dom.clientHeight
+        console.log(width.value,height.value);
+    })
+
+    return {
+        width,
+        height
+    }
+}
