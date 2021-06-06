00233bf4402f05d66c4fb7247646b663feb67b78
Nathan Tai <nathantai@outlook.com>
2021-06-06 11:28:30 +0800

    new mapTest

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,12 @@ import {createRouter,createWebHashHistory} from 'vue-router'
 // 也可以从其他文件导入
 import Home from '../view/home.vue'
 import scrollTest from '../view/scrollTest.vue'
+import mapTest from '../view/mapTest.vue'
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
 // 我们后面再讨论嵌套路由。
 const routes = [
-    { path: '/', component: Home },
+    { path: '/', component: mapTest },
 ]
 
 // 3. 创建路由实例并传递 `routes` 配置
---
+++ src/view/scrollTest.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <BaseScrollList
+                style="width: 500px;height: 500px"
+                :config="config"
+        ></BaseScrollList>
+    </div>
+</template>
+
+<script>
+    import BaseScrollList from '/@/components/BaseScrollList/index.vue'
+    import {ref} from "vue";
+
+    export default {
+        name: "scrollTest",
+        components:{BaseScrollList},
+        setup(props){
+
+            let config = ref({})
+
+            let data = []
+
+            for(let i = 0;i<10;i++){
+              data.push([
+                `同学${i+1}`,
+                Math.floor(Math.random() * 10 + 20),
+                Math.floor(Math.random() * 10000 * 10000),
+              ])
+            }
+            config.value = {
+                header:['姓名','月薪','年龄'],
+                headerStyle:[{color:'red',width:'150px'}],
+                rowStyle:[{color:'blue'}],
+                rowBg:['rgb(240,240,240)','rgb(255,255,255)'],
+                align:['center','center','left','right'],
+                headerFontSize:20,
+                rowFontSize:20,
+                headerColor:'green',
+                rowColor:'pink',
+                headerHeight:"40",
+                headerIndex:true,
+                data,
+                rowNum:8
+            }
+            return{
+                config
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
